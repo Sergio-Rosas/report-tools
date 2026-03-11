f1a9d5091b75a940731be3901bde62ae346d58f3
Sergio Rosas <gunners.src@hotmail.com>
2026-03-11 12:26:01 -0500

    Template component ready

--- src/MainForm.jsx
+++ src/MainForm.jsx
@@ -19,204 +19,212 @@ export default function MainForm({ template, handleTemplate }) {
     }
 
     return (
-        <form className="form" action={save}>
-            <div className="user">
-                <div className="radio-square--container">
-                    <input
-                        className="radio-square"
-                        type="radio"
-                        value="fabricante"
-                        id="fabricante"
-                        name="usuario"
-                        defaultChecked={name !== "En blanco" && "true"}
-                        required
-                    />
-                    <label htmlFor="fabricante">Fabricante</label>
+        <>
+            <h2 className="title title--template">{template}</h2>
+            <form className="form" action={save}>
+                <div className="user">
+                    <div className="radio-square--container">
+                        <input
+                            className="radio-square"
+                            type="radio"
+                            value="fabricante"
+                            id="fabricante"
+                            name="usuario"
+                            defaultChecked={name !== "En blanco" && "true"}
+                            required
+                        />
+                        <label htmlFor="fabricante">Fabricante</label>
+                    </div>
+                    <div className="radio-square--container">
+                        <input
+                            className="radio-square"
+                            type="radio"
+                            value="distribuidor"
+                            id="distribuidor"
+                            name="usuario"
+                            required
+                        />
+                        <label htmlFor="distribuidor">Distribuidor</label>
+                    </div>
+                    <div className="radio-square--container">
+                        <input
+                            className="radio-square"
+                            type="radio"
+                            value="consumidor"
+                            id="consumidor"
+                            name="usuario"
+                            required
+                        />
+                        <label htmlFor="consumidor">Consumidor</label>
+                    </div>
                 </div>
-                <div className="radio-square--container">
+
+                <div className="important">
+                    <label htmlFor="empresa">Nombre de la Empresa:</label>
                     <input
-                        className="radio-square"
-                        type="radio"
-                        value="distribuidor"
+                        type="text"
+                        name="empresa"
+                        id="empresa"
+                        onFocus={handleFocus}
+                        required
+                        defaultValue={defaultValue ? "Empresa Temporal" : ""}
+                    />
+
+                    <label htmlFor="fecha-fabricacion">
+                        Fecha de Fabricación:
+                    </label>
+                    <input
+                        type="date"
+                        name="fecha-fabricacion"
+                        id="fecha-fabricacion"
+                        min="2016-01-01"
+                        onFocus={handleFocus}
+                        required
+                        defaultValue={defaultValue ? "2020-11-02" : ""}
+                    />
+
+                    <label htmlFor="distribuidor">Distribuidor:</label>
+                    <input
+                        type="text"
+                        name="distribuidor"
                         id="distribuidor"
-                        name="usuario"
+                        onFocus={handleFocus}
                         required
+                        value={name === "En blanco" ? "" : distributor}
                     />
-                    <label htmlFor="distribuidor">Distribuidor</label>
-                </div>
-                <div className="radio-square--container">
+
+                    <label htmlFor="referencia">Referencia:</label>
                     <input
-                        className="radio-square"
-                        type="radio"
-                        value="consumidor"
-                        id="consumidor"
-                        name="usuario"
+                        type="text"
+                        name="referencia"
+                        id="referencia"
+                        defaultValue={reference}
+                        onFocus={handleFocus}
                         required
                     />
-                    <label htmlFor="consumidor">Consumidor</label>
-                </div>
-            </div>
 
-            <div className="important">
-                <label htmlFor="empresa">Nombre de la Empresa:</label>
-                <input
-                    type="text"
-                    name="empresa"
-                    id="empresa"
-                    onFocus={handleFocus}
-                    required
-                    defaultValue={defaultValue ? "Empresa Temporal" : ""}
-                />
-
-                <label htmlFor="fecha-fabricacion">Fecha de Fabricación:</label>
-                <input
-                    type="date"
-                    name="fecha-fabricacion"
-                    id="fecha-fabricacion"
-                    min="2016-01-01"
-                    onFocus={handleFocus}
-                    required
-                    defaultValue={defaultValue ? "2020-11-02" : ""}
-                />
-
-                <label htmlFor="distribuidor">Distribuidor:</label>
-                <input
-                    type="text"
-                    name="distribuidor"
-                    id="distribuidor"
-                    onFocus={handleFocus}
-                    required
-                    value={name === "En blanco" ? "" : distributor}
-                />
-
-                <label htmlFor="referencia">Referencia:</label>
-                <input
-                    type="text"
-                    name="referencia"
-                    id="referencia"
-                    defaultValue={reference}
-                    onFocus={handleFocus}
-                    required
-                />
-
-                <label htmlFor="fecha-inspeccion">Fecha de Inspección:</label>
-                <input
-                    type="date"
-                    name="fecha-inspeccion"
-                    id="fecha-inspeccion"
-                    min="2026-01-01"
-                    onFocus={handleFocus}
-                    required
-                    defaultValue={
-                        defaultValue
-                            ? new Date().toISOString().split("T")[0]
-                            : ""
-                    }
-                />
-
-                <label htmlFor="lote">Lote:</label>
-                <input
-                    type="text"
-                    name="lote"
-                    id="lote"
-                    onFocus={handleFocus}
-                    required
-                    defaultValue={defaultValue ? "200T89756" : ""}
-                />
-
-                <label htmlFor="producto">Nombre del Producto:</label>
-                <input
-                    type="text"
-                    name="producto"
-                    id="producto"
-                    defaultValue={name === "En blanco" ? "" : name}
-                    onFocus={handleFocus}
-                    required
-                />
-
-                <label htmlFor="serie">Serie:</label>
-                <input
-                    type="text"
-                    name="serie"
-                    id="serie"
-                    onFocus={handleFocus}
-                    required
-                    defaultValue={defaultValue ? "7898852" : ""}
-                />
-            </div>
-            <div className="flex-container">
-                <h3>FORMATO DE INSPECCIÓN DE EQUIPOS</h3>
-                <p>
-                    Aspectos a inspeccionar: etiquetas, reatas, cuerdas,
-                    costuras, herrajes, plásticos
-                </p>
-            </div>
-            <div className="table-container">
-                {conditions.map((obj, index) => (
-                    <Table
-                        title={`${obj.title}`}
-                        conditions={obj.elems}
-                        index={index + 1}
-                        key={obj.title}
-                    />
-                ))}
-            </div>
-            <div>
-                <p>
-                    <span className="subtitle">Continúa en servicio:</span>{" "}
-                    Equipo que de acuerdo a la inspección puede seguir en uso.
-                </p>
-                <p>
-                    <span className="subtitle">Retirar en servicio:</span>{" "}
-                    Equipo dado de baja.
-                </p>
-            </div>
-
-            <div className="veredict">
-                <p className="subtitle">Veredicto:</p>
-                <div className="radio-square--container">
+                    <label htmlFor="fecha-inspeccion">
+                        Fecha de Inspección:
+                    </label>
                     <input
-                        className="radio-square"
-                        type="radio"
-                        value="continua"
-                        id="continua"
-                        name="servicio"
-                        defaultChecked={name !== "En blanco" && "true"}
+                        type="date"
+                        name="fecha-inspeccion"
+                        id="fecha-inspeccion"
+                        min="2026-01-01"
+                        onFocus={handleFocus}
                         required
+                        defaultValue={
+                            defaultValue
+                                ? new Date().toISOString().split("T")[0]
+                                : ""
+                        }
                     />
-                    <label htmlFor="continua">Continúa en servicio</label>
-                </div>
-                <div className="radio-square--container">
-                    <input
-                        className="radio-square"
-                        type="radio"
-                        value="retirar"
-                        id="retirar"
-                        name="servicio"
-                        required
-                    />
-                    <label htmlFor="retirar">Retirar de servicio</label>
-                </div>
-            </div>
 
-            <label className="subtitle" htmlFor="informe">
-                Informe final del equipo/conclusiones:
-            </label>
-            <textarea
-                name="informe"
-                id="informe"
-                required
-                defaultValue={name === "En blanco" ? "" : conclusions}
-            ></textarea>
-            <div className="buttons">
-                <input
-                    type="button"
-                    value="↵ Regresar"
-                    className="button button--back"
-                    onClick={() => handleTemplate(false)}
-                />
-                <button className="button button--save">Guardar</button>
-            </div>
-        </form>
+                    <label htmlFor="lote">Lote:</label>
+                    <input
+                        type="text"
+                        name="lote"
+                        id="lote"
+                        onFocus={handleFocus}
+                        required
+                        defaultValue={defaultValue ? "200T89756" : ""}
+                    />
+
+                    <label htmlFor="producto">Nombre del Producto:</label>
+                    <input
+                        type="text"
+                        name="producto"
+                        id="producto"
+                        defaultValue={name === "En blanco" ? "" : name}
+                        onFocus={handleFocus}
+                        required
+                    />
+
+                    <label htmlFor="serie">Serie:</label>
+                    <input
+                        type="text"
+                        name="serie"
+                        id="serie"
+                        onFocus={handleFocus}
+                        required
+                        defaultValue={defaultValue ? "7898852" : ""}
+                    />
+                </div>
+                <div className="flex-container">
+                    <h3>FORMATO DE INSPECCIÓN DE EQUIPOS</h3>
+                    <p>
+                        Aspectos a inspeccionar: etiquetas, reatas, cuerdas,
+                        costuras, herrajes, plásticos
+                    </p>
+                </div>
+                <div className="table-container">
+                    {conditions.map((obj, index) => (
+                        <Table
+                            title={`${obj.title}`}
+                            conditions={obj.elems}
+                            index={index + 1}
+                            key={obj.title}
+                        />
+                    ))}
+                </div>
+                <div>
+                    <p>
+                        <span className="subtitle">Continúa en servicio:</span>{" "}
+                        Equipo que de acuerdo a la inspección puede seguir en
+                        uso.
+                    </p>
+                    <p>
+                        <span className="subtitle">Retirar en servicio:</span>{" "}
+                        Equipo dado de baja.
+                    </p>
+                </div>
+
+                <div className="veredict">
+                    <p className="subtitle">Veredicto:</p>
+                    <div className="radio-square--container">
+                        <input
+                            className="radio-square"
+                            type="radio"
+                            value="continua"
+                            id="continua"
+                            name="servicio"
+                            defaultChecked={name !== "En blanco" && "true"}
+                            required
+                        />
+                        <label htmlFor="continua">Continúa en servicio</label>
+                    </div>
+                    <div className="radio-square--container">
+                        <input
+                            className="radio-square"
+                            type="radio"
+                            value="retirar"
+                            id="retirar"
+                            name="servicio"
+                            required
+                        />
+                        <label htmlFor="retirar">Retirar de servicio</label>
+                    </div>
+                </div>
+
+                <label className="subtitle" htmlFor="informe">
+                    Informe final del equipo/conclusiones:
+                </label>
+                <textarea
+                    name="informe"
+                    id="informe"
+                    required
+                    defaultValue={name === "En blanco" ? "" : conclusions}
+                ></textarea>
+                <div className="buttons">
+                    <input
+                        type="button"
+                        value="↵ Regresar"
+                        className="button button--back"
+                        onClick={() => handleTemplate(false)}
+                    />
+                    <button className="button button--save">Guardar</button>
+                </div>
+            </form>
+        </>
     );
 }
--- src/Templates.jsx
+++ src/Templates.jsx
@@ -7,40 +7,47 @@ export default function Templates({ handleTemplate }) {
     }
 
     return (
-        <form action={processTemplate} className="select-form">
-            <select
-                name="template"
-                id="template"
-                onChange={(e) => (selectedTemplate.current = e.target.value)}
+        <div className="form-container">
+            <form
+                action={processTemplate}
+                className="select-form select-form--template"
             >
-                <option value="seleccion" disabled selected>
-                    Seleccione una plantilla
-                </option>
-                <option value="Adaptador de anclaje">
-                    Adaptador de anclaje
-                </option>
-                <option value="Arnes">Arnes</option>
-                <option value="Cuerda semiestatica de 11mm">
-                    Cuerda semiestatica de 11mm
-                </option>
-                <option value="En blanco">En blanco</option>
-                <option value="Eslinga con absorbedor de impacto">
-                    Eslinga con absorbedor de impacto
-                </option>
-                <option value="Eslinga de posicionamiento">
-                    Eslinga de posicionamiento
-                </option>
-                <option value="Freno arrestador automatico">
-                    Freno arrestador automatico
-                </option>
-                <option value="Mosqueton carabinero automatico">
-                    Mosqueton carabinero automatico
-                </option>
-                <option value="Polea">Polea</option>
-            </select>
-            <div className="buttons">
-                <button className="button button--save">Acceder</button>
-            </div>
-        </form>
+                <select
+                    name="template"
+                    id="template"
+                    onChange={(e) =>
+                        (selectedTemplate.current = e.target.value)
+                    }
+                >
+                    <option value="seleccion" disabled selected>
+                        Seleccione una plantilla
+                    </option>
+                    <option value="Adaptador de anclaje">
+                        Adaptador de anclaje
+                    </option>
+                    <option value="Arnes">Arnes</option>
+                    <option value="Cuerda semiestatica de 11mm">
+                        Cuerda semiestatica de 11mm
+                    </option>
+                    <option value="En blanco">En blanco</option>
+                    <option value="Eslinga con absorbedor de impacto">
+                        Eslinga con absorbedor de impacto
+                    </option>
+                    <option value="Eslinga de posicionamiento">
+                        Eslinga de posicionamiento
+                    </option>
+                    <option value="Freno arrestador automatico">
+                        Freno arrestador automatico
+                    </option>
+                    <option value="Mosqueton carabinero automatico">
+                        Mosqueton carabinero automatico
+                    </option>
+                    <option value="Polea">Polea</option>
+                </select>
+                <div className="buttons">
+                    <button className="button button--save">Acceder</button>
+                </div>
+            </form>
+        </div>
     );
 }
